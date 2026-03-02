e1ad0a1ecc9d59cdf2462120861acf6940f4ada2
Andras Bacsai <5845193+andrasbacsai@users.noreply.github.com>
2026-03-02 08:24:55 +0100

    fix(mail): render email verification URL without HTML escaping
    Change Blade template syntax from {{ }} to {!! !!} to prevent URL
    escaping in the email change verification template. This ensures the
    verification URL renders correctly in the email without encoded
    characters breaking the link.

--- resources/views/mail/email-change.blade.php
+++ resources/views/mail/email-change.blade.php
@@ -3,7 +3,7 @@ Verify Email Change
 Hi {{ $userName }},
 
 You requested to change your email address. Please verify this new email by visiting the link below:
-{{ $verifyUrl }}
+{!! $verifyUrl !!}
 
 This link will expire in 15 minutes. If you did not request this change, you may discard this email.
 
